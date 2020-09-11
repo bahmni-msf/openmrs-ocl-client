4d36de697df402ff24b9e5c1754e5eb24280d612
Rakesh Kumar <rakesh.kumar@thoughtworks.com>
2020-09-11 17:53:49 +0530

    Rakesh| MOBN-1444 | Setup view concepts page

--- src/apps/sources/Routes.tsx
+++ src/apps/sources/Routes.tsx
@@ -1,31 +1,45 @@
 import React from "react";
-import {Route, Switch, useRouteMatch} from "react-router-dom";
-import {ViewSourcePage, EditSourcePage} from "./pages";
+import { Route, Switch, useRouteMatch } from "react-router-dom";
+import { ViewSourcePage, EditSourcePage } from "./pages";
+import ConceptRoutes from "../concepts/Routes";
+import { SOURCE_VERSION_CONTAINER } from "../concepts/constants";
 
 interface Props {
-    viewSource?: boolean;
-    editSource?: boolean;
+  viewSource?: boolean;
+  editSource?: boolean;
+  concepts: boolean;
 }
 
 const Routes: React.FC<Props> = ({
-     viewSource = true,
-     editSource = true
-                                 }) => {
-    let { path } = useRouteMatch();
-    return (
-        <Switch>
-            {!viewSource ? null : (
-                <Route exact path={`${path}/:source/`}>
-                    <ViewSourcePage />
-                </Route>
-            )}
-            {!editSource ? null : (
-                <Route exact path={`${path}/:source/edit/`}>
-                    <EditSourcePage />
-                </Route>
-            )}
-        </Switch>
-    );
+  viewSource = true,
+  editSource = true,
+  concepts = true,
+}) => {
+  let { path } = useRouteMatch();
+  return (
+    <Switch>
+      {!viewSource ? null : (
+        <Route exact path={`${path}/:source/`}>
+          <ViewSourcePage />
+        </Route>
+      )}
+      {!editSource ? null : (
+        <Route exact path={`${path}/:source/edit/`}>
+          <EditSourcePage />
+        </Route>
+      )}
+      {/* {!concepts ? null : (
+        <>
+          <Route path={`${path}/:collection/:version/concepts/`}>
+            <ConceptRoutes
+              containerType={SOURCE_VERSION_CONTAINER}
+              viewConcepts={true}
+            />
+          </Route>
+        </>
+      )} */}
+    </Switch>
+  );
 };
 
 export default Routes;
