8d2e039eaa184cd2c18c033d0e003e58af186421
Supriya Muppiri <supriya.muppiri@thoughtworks.com>
2020-09-15 19:51:42 +0530

    Tarun, Supriya | MOBN-1428 | Add unit test cases for userform

--- src/apps/authentication/__test__/components/UserForm.test.tsx
+++ src/apps/authentication/__test__/components/UserForm.test.tsx
@@ -25,4 +25,41 @@ describe("UserForm", () => {
 
         expect(container).toMatchSnapshot();
     });
+    it('should show correct User Name', () => {
+        const {getByLabelText} = renderUI();
+        const header = getByLabelText('User Name');
+
+        expect(header.textContent).toBe("TestUser");
+    });
+    it('should show correct Full name', () => {
+        const {getByLabelText} = renderUI();
+        const header = getByLabelText('Full Name');
+
+        expect(header.textContent).toBe("TestUser");
+    });
+    it('should show correct Email ID', () => {
+        const {getByLabelText} = renderUI();
+        const header = getByLabelText('Email ID');
+
+        expect(header.textContent).toBe("TestUser@test.com");
+    });
+    it('should show correct Company', () => {
+        const {getByLabelText} = renderUI();
+        const header = getByLabelText('Company');
+
+        expect(header.textContent).toBe("Test Company");
+    });
+    it('should show correct Location', () => {
+        const {getByLabelText} = renderUI();
+        const header = getByLabelText('Location');
+
+        expect(header.textContent).toBe("Test Location");
+    });
+    it('should show correct Joined Date', () => {
+        const {getByLabelText} = renderUI();
+        const header = getByLabelText('Joined Date');
+
+        // @ts-ignore
+        expect(header.value).toBe("01 Jan 1900");
+    });
 });
