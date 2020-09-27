03f545304dac8139b2ed328840af4bac5e2ef570
Rakesh Kumar <rakesh.kumar@thoughtworks.com>
2020-09-27 12:02:11 +0530

    Rakesh, Tarun | MOBN-1429 | Progress Notification - View more details at concept level
    Co-authored-by: tarunkumar-tw <tarunkumar.s@thoughtworks.com>

---
+++ src/apps/notifications/__test__/NotificationDetails.test.tsx
@@ -0,0 +1,148 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import NotificationDetails from "../components/NotificationDetails";
+import {
+  NotificationItem,
+  NotificationItemRow
+} from "../types";
+
+type notificationDetailsProps = React.ComponentProps<typeof NotificationDetails>;
+
+const notificationItemRow: NotificationItemRow ={
+  expression: "/orgs/testOrg/sources/testSource/concepts/testConceptID/",
+  added: true,
+  message: "concept imported successfully"
+};
+const notificationItem: NotificationItem = {
+  result: [notificationItemRow],
+  progress: "",
+  meta: ["/users/testUser/collections/testDictionary/"]
+};
+
+const handleClose = jest.fn();
+const baseProps: notificationDetailsProps = {
+  open: true,
+  handleClose: handleClose,
+  notification: notificationItem
+};
+
+function renderUI(props: Partial<notificationDetailsProps> = {}) {
+  return render(
+    <NotificationDetails {...baseProps} {...props} />
+  );
+}
+
+describe("NotificationDetails", () => {
+
+  it("should show the correct title", () => {
+    const { queryByTestId } = renderUI();
+    const title = queryByTestId('title') || {textContent:null};
+
+    expect(title.textContent).toBe("testDictionary - Adding concepts from testSource");
+  });
+
+  it("should hide dialog on click of close button", () => {
+    const { getByText } = renderUI();
+    const closeButton = getByText('Close');
+    fireEvent.click(closeButton);
+    expect(handleClose).toHaveBeenCalledTimes(1);
+  });
+
+});
+
+describe("Notification Summary Table", () => {
+
+  const notificationItemRow1: NotificationItemRow ={
+    expression: "/orgs/testOrg/sources/testSource/concepts/testConceptID1/",
+    added: true,
+    message: "concept imported successfully"
+  };
+
+  const notificationItemRow2: NotificationItemRow ={
+    expression: "/orgs/testOrg/sources/testSource/concepts/testConceptID2/",
+    added: false,
+    message: "concept import failed due to conflicts"
+  };
+
+  const notificationItemRow3: NotificationItemRow ={
+    expression: "/orgs/testOrg/sources/testSource/concepts/testDependentConceptID1/",
+    added: true,
+    message: "concept imported successfully"
+  };
+
+  const notificationItemRow4: NotificationItemRow ={
+    expression: "/orgs/testOrg/sources/testSource/concepts/testDependentConceptID2/",
+    added: false,
+    message: "concept import failed due to conflicts"
+  };
+
+  const notificationItem: NotificationItem = {
+    result: [notificationItemRow1, notificationItemRow2, notificationItemRow3, notificationItemRow4],
+    progress: "",
+    meta: ["/users/testUser/collections/testDictionary/", [{id: "testConceptID1"}, {id: "testConceptID2"}]]
+  };
+
+  let queryByTestId: Function;
+  beforeEach(() => {
+    const queries = renderUI({
+      notification: notificationItem
+    });
+    queryByTestId = queries.queryByTestId;
+  });
+
+  it("should have the correct table headers", () => {
+    const enhancedTableHead = queryByTestId('enhancedTableHead');
+    const enhancedTableHeadData = enhancedTableHead ? enhancedTableHead.querySelectorAll("th"): [];
+
+    expect(enhancedTableHeadData.length).toEqual(4);
+    expect(enhancedTableHeadData[0].textContent).toBe("Concept ID");
+    expect(enhancedTableHeadData[1].textContent).toBe("Concept Type");
+    expect(enhancedTableHeadData[2].textContent).toBe("Statussorted descending");
+    expect(enhancedTableHeadData[3].textContent).toBe("Reasons");
+  });
+
+  it("should have the correct table data in appropriate columns - Parent Concept Imported", () => {
+    const tableRow = queryByTestId('testConceptID1');
+    const tableRowData = tableRow ? tableRow.querySelectorAll("td"): [];
+
+    expect(tableRowData.length).toEqual(4);
+    expect(tableRowData[0].textContent).toBe("testConceptID1");
+    expect(tableRowData[1].textContent).toBe("Parent");
+    expect(tableRowData[2].textContent).toBe("Imported");
+    expect(tableRowData[3].textContent).toBe("concept imported successfully");
+  });
+
+  it("should have the correct table data in appropriate columns - Parent Concept Failed", () => {
+    const tableRow = queryByTestId('testConceptID2');
+    const tableRowData = tableRow ? tableRow.querySelectorAll("td"): [];
+
+    expect(tableRowData.length).toEqual(4);
+    expect(tableRowData[0].textContent).toBe("testConceptID2");
+    expect(tableRowData[1].textContent).toBe("Parent");
+    expect(tableRowData[2].textContent).toBe("Skipped");
+    expect(tableRowData[3].textContent).toBe("concept import failed due to conflicts");
+  });
+
+  it("should have the correct table data in appropriate columns - Dependent Concept Imported", () => {
+    const tableRow = queryByTestId('testDependentConceptID1');
+    const tableRowData = tableRow ? tableRow.querySelectorAll("td"): [];
+
+    expect(tableRowData.length).toEqual(4);
+    expect(tableRowData[0].textContent).toBe("testDependentConceptID1");
+    expect(tableRowData[1].textContent).toBe("Dependent");
+    expect(tableRowData[2].textContent).toBe("Imported");
+    expect(tableRowData[3].textContent).toBe("concept imported successfully");
+  });
+
+  it("should have the correct table data in appropriate columns - Dependent Concept Failed", () => {
+    const tableRow = queryByTestId('testDependentConceptID2');
+    const tableRowData = tableRow ? tableRow.querySelectorAll("td"): [];
+
+    expect(tableRowData.length).toEqual(4);
+    expect(tableRowData[0].textContent).toBe("testDependentConceptID2");
+    expect(tableRowData[1].textContent).toBe("Dependent");
+    expect(tableRowData[2].textContent).toBe("Skipped");
+    expect(tableRowData[3].textContent).toBe("concept import failed due to conflicts");
+  });
+});
---
+++ src/apps/notifications/components/NotificationDetails.tsx
@@ -0,0 +1,234 @@
+import React from "react";
+import {
+  Button,
+  ButtonGroup,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  makeStyles,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Typography,
+  TablePagination
+} from "@material-ui/core";
+import {
+  NotificationItemRow,
+  NotificationItem
+} from "../types";
+import { EnhancedNotificationSummaryTableHead } from "./EnhancedNotificationSummaryTableHead"
+
+interface Props {
+  open: boolean;
+  handleClose: () => void;
+  notification: NotificationItem;
+}
+
+const useStyles = makeStyles({
+  dialogTitle: {
+    textAlign: "center",
+    textTransform: "capitalize"
+  },
+  tableContainer: {
+    display: "contents"
+  },
+  noPadding: {
+    padding: "0px"
+  }
+});
+
+const NotificationDetails: React.FC<Props> = ({ open, handleClose, notification }) => {
+
+  const classes = useStyles();
+
+  const getParentConceptIds = () => {
+    const meta = notification.meta;
+    if(meta && meta.length > 1) {
+      const requestPayload = meta[1];
+      const conceptIds = requestPayload.map((concept: { id: string }) => concept.id);
+      return conceptIds;
+    } else {
+      return [];
+    }
+  };
+  const parentConceptIds = getParentConceptIds();
+
+  const getConceptId = (row: NotificationItemRow) => {
+    return row.expression.split("/")[6];
+  };
+
+  const getConceptType = (row: NotificationItemRow) => {
+    const getActualIndex = (ele: string) => ele ===  getConceptId(row);
+    const actualIndex = parentConceptIds.findIndex(getActualIndex);
+    if (actualIndex > -1) {
+      return "Parent";
+    } else {
+      return "Dependent";
+    }
+  };
+
+  const getStatus = (row: NotificationItemRow) => {
+    return row.added ? "Imported" : "Skipped";
+  };
+
+  const getReason = (row: NotificationItemRow) => {
+    if (typeof row.message === "string") {
+      return row.message;
+    }
+    return row.message.join(" ");
+  };
+
+  const getDialogTitle = () => {
+    const dictionaryName = notification.meta ? notification.meta[0].split("/")[4] : "";
+
+    const sourceName = notification.result.length > 0 ? notification.result[0].expression.split("/")[4] : "";
+    return (
+        <Typography variant='h5'>
+          {dictionaryName} - Adding concepts from {sourceName}
+        </Typography>
+    );
+  };
+
+  const createData = (
+      conceptId: string,
+      conceptType: string,
+      status: string,
+      reasons: string,
+  ) => {
+    return { conceptId, conceptType, status, reasons };
+  };
+
+  const getSummaryRowsToDisplay = () => {
+    const conceptRows = notification.result.filter(row =>
+        row.expression.includes("/concepts/")
+    );
+    return conceptRows.map((row) => {
+      return createData(getConceptId(row), getConceptType(row), getStatus(row), getReason(row));
+    });
+  };
+
+  type Order = 'asc' | 'desc';
+
+  const [order, setOrder] = React.useState<Order>("desc");
+  const [orderBy, setOrderBy] = React.useState("status");
+  const handleRequestSort = (event: React.MouseEvent<unknown>, property: string) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+    setPage(0);
+  };
+
+  function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
+  }
+
+  function getComparator<Key extends keyof any>(
+      order: Order,
+      orderBy: Key,
+  ): (a: { [key in Key]: number | string }, b: { [key in Key]: number | string }) => number {
+    return order === 'desc'
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy);
+  }
+
+  function stableSort<T>(array: T[], comparator: (a: T, b: T) => number) {
+    const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) return order;
+      return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+  }
+
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+  const resetSortingAndPagination = () => {
+    setOrder("desc");
+    setOrderBy("status");
+    setPage(0);
+    setRowsPerPage(5);
+  };
+
+  const resetAndClose = () => {
+    resetSortingAndPagination();
+    handleClose();
+  };
+
+  const getTableContent = () => {
+    return <TableContainer component={Paper} className={classes.tableContainer}>
+      <Table stickyHeader>
+        <EnhancedNotificationSummaryTableHead
+            order={order}
+            orderBy={orderBy}
+            onRequestSort={handleRequestSort}
+        />
+        <TableBody>
+          {stableSort(getSummaryRowsToDisplay(), getComparator(order, orderBy))
+            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            .map((row) => (
+              <TableRow key={row.conceptId} data-testid={row.conceptId}>
+                <TableCell>{row.conceptId}</TableCell>
+                <TableCell>{row.conceptType}</TableCell>
+                <TableCell>{row.status}</TableCell>
+                <TableCell>{row.reasons}</TableCell>
+              </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>;
+  };
+
+  const getTablePagination = () => {
+    return <TablePagination
+        rowsPerPageOptions={[5, 10, 25, 50, 100]}
+        component="div"
+        count={getSummaryRowsToDisplay().length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+    />;
+  };
+
+  return (
+      <>
+        <Dialog data-testid="notification-details-dialog" onClose={resetAndClose} open={open} fullWidth maxWidth = {'md'}>
+          <DialogTitle data-testid="title" className={classes.dialogTitle} disableTypography>
+            {getDialogTitle()}
+          </DialogTitle>
+          <DialogContent dividers className={classes.noPadding}>
+            {getTableContent()}
+            {getTablePagination()}
+          </DialogContent>
+          <DialogActions>
+            <ButtonGroup fullWidth color="primary" variant="text" size="medium">
+              <Button>
+                Export To CSV
+              </Button>
+              <Button onClick={resetAndClose} color="secondary">Close</Button>
+            </ButtonGroup>
+          </DialogActions>
+        </Dialog>
+      </>
+  );
+};
+
+export default NotificationDetails;
